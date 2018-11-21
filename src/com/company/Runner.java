59144e5a1b02c0867fe7ffee2ddc395332694d2f
Z Chen <zchen6464@bths.edu>
2018-11-21 10:44:00 -0500

    Create Runner.java

---
+++ src/com/company/Runner.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Runner {
+
+    public static void Runner(String[] args) {
+        int[] test = {1,4,6,5,7};
+        mergeSort(test);
+        System.out.println(Arrays.toString(test));
+    }
